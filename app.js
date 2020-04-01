8a83c9b3a711c020aa0944f3c89f248b120fa310
MasanoriIwakura <hagobutler@yahoo.co.jp>
2020-04-01 10:21:33 +0900

    Add .env settings

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
+require("dotenv").config();
+
+app.set("port", process.env.PORT || 5050);
 app.get("/", (req, res) => res.send("Hello"));
 
-app.listen(3000, () =>
-  console.log("cli-table-sandbox app listening on port 3000!")
+const port = app.get("port");
+app.listen(port, () =>
+  console.log(`cli-table-sandbox app listening on port ${port}!`)
 );
